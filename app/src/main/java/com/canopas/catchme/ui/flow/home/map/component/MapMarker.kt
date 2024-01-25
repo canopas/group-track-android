e0bbe871383ba5a35fc2d9669f3d02d3722d2a36
Radhika Canopas <radhika.s@canopas.com>
2024-01-25 11:22:02 +0530

    Refactor marker

--- app/src/main/java/com/canopas/catchme/ui/flow/home/map/component/MapMarker.kt
+++ app/src/main/java/com/canopas/catchme/ui/flow/home/map/component/MapMarker.kt
@@ -3,6 +3,7 @@ package com.canopas.catchme.ui.flow.home.map.component
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
 import com.canopas.catchme.data.models.location.ApiLocation
 import com.canopas.catchme.data.models.user.ApiUser
 import com.google.android.gms.maps.model.LatLng
@@ -26,6 +27,7 @@ fun MapMarker(
         state = markerState,
         title = user.fullName,
         icon = iconState,
+        anchor = Offset(0.0f, 1f),
         onClick = {
             onClick()
             false
--- app/src/main/java/com/canopas/catchme/ui/flow/home/map/component/MarkerIcon.kt
+++ app/src/main/java/com/canopas/catchme/ui/flow/home/map/component/MarkerIcon.kt
@@ -5,9 +5,9 @@ import android.graphics.Bitmap
 import android.graphics.BitmapShader
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.CornerPathEffect
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.RectF
 import android.graphics.Shader
 import android.graphics.Typeface
 import android.graphics.drawable.BitmapDrawable
@@ -20,9 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.SuccessResult
@@ -34,12 +31,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlin.math.roundToInt
 
+private const val MARKER_ICON_SIZE = 60f
 @Composable
 fun rememberMarkerIconState(user: ApiUser): BitmapDescriptor? {
     val context = LocalContext.current
     val markerColor = AppTheme.colorScheme.primary.toArgb()
-    val markerIconSize = with(LocalDensity.current) { 60.dp.toPx() }
-    val markerTextSize = with(LocalDensity.current) { 28.sp.toPx() }
 
     var iconState by remember { mutableStateOf<BitmapDescriptor?>(null) }
 
@@ -47,9 +43,7 @@ fun rememberMarkerIconState(user: ApiUser): BitmapDescriptor? {
         iconState = loadBitmapDescriptorFromUrl(
             context,
             user,
-            markerColor,
-            markerIconSize,
-            markerTextSize
+            markerColor
         )
     }
     return iconState
@@ -59,63 +53,67 @@ private suspend fun loadBitmapDescriptorFromUrl(
     context: Context,
     apiUser: ApiUser,
     markerColor: Int,
-    markerIconSize: Float,
-    markerTextSize: Float
 ): BitmapDescriptor {
     val imageUrl = apiUser.profile_image
     val userName = apiUser.first_name?.first()?.toString() ?: ""
+    val dm = context.resources.displayMetrics
+    val markerIconSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, MARKER_ICON_SIZE, dm).roundToInt()
+
     return withContext(Dispatchers.IO) {
         if (imageUrl.isNullOrEmpty()) {
             val placeHolder = createPlaceHolderBitmap(
+                context,
                 userName,
                 markerColor,
-                markerIconSize.roundToInt(),
-                markerTextSize
             )
 
-            transformBitmap(context, placeHolder, markerColor, markerIconSize)
+            transformBitmap(context, placeHolder)
         } else {
             val loader = ImageLoader(context)
             val request = ImageRequest.Builder(context)
                 .data(imageUrl)
                 .allowHardware(false)
-                .size(markerIconSize.roundToInt())
+                .size(markerIconSize)
                 .build()
 
             val result = loader.execute(request)
             val drawable = (result as? SuccessResult)?.drawable
             val bitmap = (drawable as? BitmapDrawable)?.bitmap ?: createPlaceHolderBitmap(
+                context,
                 userName,
                 markerColor,
-                markerIconSize.roundToInt(),
-                markerTextSize
             )
 
             transformBitmap(
                 context,
-                bitmap,
-                markerColor,
-                markerIconSize
+                bitmap
             )
         }
     }
 }
 
 private fun createPlaceHolderBitmap(
+    context: Context,
     markerLabel: String,
     markerColor: Int,
-    markerIconSize: Int,
-    markerTextSize: Float
 ): Bitmap {
+    val dm = context.resources.displayMetrics
+    val markerIconSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, MARKER_ICON_SIZE, dm).roundToInt()
+    val markerTextSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 28f, dm)
+    val corner = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16f, dm)
     val output = Bitmap.createBitmap(markerIconSize, markerIconSize, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(output)
-    val r = markerIconSize / 2f
 
-    val whiteBorder = Paint()
-    whiteBorder.isAntiAlias = true
-    whiteBorder.color = markerColor
-    whiteBorder.alpha = 180
-    canvas.drawCircle(r, r, r, whiteBorder)
+    val bgPaint = Paint()
+    bgPaint.isAntiAlias = true
+    bgPaint.color = markerColor
+    bgPaint.alpha = 180
+    canvas.drawRoundRect(
+        RectF(0f, 0f, markerIconSize.toFloat(), markerIconSize.toFloat()),
+        corner,
+        corner,
+        bgPaint
+    )
 
     val paint = Paint()
     paint.color = Color.WHITE
@@ -134,56 +132,58 @@ private fun createPlaceHolderBitmap(
 
 private fun transformBitmap(
     context: Context,
-    source: Bitmap,
-    markerColor: Int,
-    markerIconSize: Float
+    source: Bitmap
 ): BitmapDescriptor {
     val dm = context.resources.displayMetrics
 
-    val photoMargin = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 6f, dm)
-    val strokeWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3f, dm)
-    val whiteStrokeWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4.5f, dm)
+    val size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, MARKER_ICON_SIZE, dm).roundToInt()
+    val photoMargin = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4f, dm)
+    val corner = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16f, dm)
+    val pointerCorner = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3f, dm)
 
-    val triangleMargin = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5f, dm)
-    val size = markerIconSize.roundToInt()
+    val shadowMargin = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1f, dm)
 
-    val r = size / 2f
     val output =
         Bitmap.createBitmap(
-            (size + triangleMargin).roundToInt(),
-            (size + triangleMargin).roundToInt(),
+            (size + shadowMargin + shadowMargin).roundToInt(),
+            (size + shadowMargin + shadowMargin).roundToInt(),
             Bitmap.Config.ARGB_8888
         )
     val canvas = Canvas(output)
 
-    val trianglePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    trianglePaint.color = markerColor
-    trianglePaint.style = Paint.Style.FILL_AND_STROKE
-    trianglePaint.isAntiAlias = true
-    trianglePaint.pathEffect = CornerPathEffect(15f)
+    val corners = floatArrayOf(
+        corner, corner,
+        corner, corner,
+        corner, corner,
+        pointerCorner, pointerCorner
+    )
 
-    val triangle = Path()
-    triangle.fillType = Path.FillType.EVEN_ODD
-    triangle.moveTo(size - strokeWidth, size / 2f)
-    triangle.lineTo(size / 2f, size + triangleMargin)
-    triangle.lineTo(strokeWidth, size / 2f)
-    triangle.close()
-    canvas.drawPath(triangle, trianglePaint)
+    val bgPaint = Paint()
 
-    val paintBorder = Paint()
-    paintBorder.isAntiAlias = true
-    paintBorder.color = markerColor
-    canvas.drawCircle(r, r, r - strokeWidth, paintBorder)
+    bgPaint.isAntiAlias = true
+    bgPaint.color = Color.WHITE
 
-    val whiteBorder = Paint()
-    whiteBorder.isAntiAlias = true
-    whiteBorder.color = Color.WHITE
-    canvas.drawCircle(r, r, r - whiteStrokeWidth, whiteBorder)
+    bgPaint.setShadowLayer(shadowMargin, 0f, 0f, Color.GRAY);
+
+    val rect = RectF(
+        shadowMargin,
+        shadowMargin,
+        size.toFloat() - shadowMargin,
+        size.toFloat() - shadowMargin
+    )
+    val path = Path()
+    path.addRoundRect(rect, corners, Path.Direction.CW)
+    canvas.drawPath(path, bgPaint)
 
     val paint = Paint()
     paint.isAntiAlias = true
     paint.setShader(BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP))
-    canvas.drawCircle(r, r, r - photoMargin, paint)
+    canvas.drawRoundRect(
+        RectF(photoMargin, photoMargin, size - photoMargin, size - photoMargin),
+        corner,
+        corner,
+        paint
+    )
 
     if (source != output) {
         source.recycle()
