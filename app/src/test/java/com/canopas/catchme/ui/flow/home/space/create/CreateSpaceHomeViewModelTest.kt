f315bdb66417339efc0cd8404a947dce264ad363
Radhika Canopas <radhika.s@canopas.com>
2024-01-30 11:21:25 +0530

    Add tests

---
+++ app/src/test/java/com/canopas/catchme/ui/flow/home/space/create/CreateSpaceHomeViewModelTest.kt
@@ -0,0 +1,77 @@
+package com.canopas.catchme.ui.flow.home.space.create
+
+import com.canopas.catchme.MainCoroutineRule
+import com.canopas.catchme.data.repository.SpaceRepository
+import com.canopas.catchme.data.utils.AppDispatcher
+import com.canopas.catchme.ui.navigation.HomeNavigator
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@ExperimentalCoroutinesApi
+class CreateSpaceHomeViewModelTest {
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    private val spaceRepository = mock<SpaceRepository>()
+    private val testDispatcher = AppDispatcher(IO = UnconfinedTestDispatcher())
+    private val appNavigator = mock<HomeNavigator>()
+
+    private lateinit var viewModel: CreateSpaceHomeViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = CreateSpaceHomeViewModel(
+            appNavigator = appNavigator,
+            spaceRepository = spaceRepository,
+            appDispatcher = testDispatcher
+        )
+    }
+
+    @Test
+    fun `navigateBack should call appNavigator to navigateBack`() {
+        viewModel.navigateBack()
+        verify(appNavigator).navigateBack()
+    }
+
+    @Test
+    fun `onSpaceNameChange should update state with spaceName`() {
+        viewModel.onSpaceNameChange("spaceName")
+        assert(viewModel.state.value.spaceName == "spaceName")
+    }
+
+    @Test
+    fun `createSpace should call spaceRepository to createSpaceAndGetInviteCode`() = runTest {
+        viewModel.onSpaceNameChange("spaceName")
+        viewModel.createSpace()
+        verify(spaceRepository).createSpaceAndGetInviteCode("spaceName")
+    }
+
+    @Test
+    fun `createSpace should call appNavigator to navigateTo SpaceInvitation`() = runTest {
+        whenever(spaceRepository.createSpaceAndGetInviteCode("spaceName"))
+            .thenReturn("inviteCode")
+        viewModel.onSpaceNameChange("spaceName")
+        viewModel.createSpace()
+        verify(appNavigator).navigateTo(
+            "space-invite/inviteCode/spaceName",
+            "create-space",
+            true
+        )
+    }
+
+    @Test
+    fun `createSpace should update state with error when spaceRepository throws exception`() = runTest {
+        whenever(spaceRepository.createSpaceAndGetInviteCode("spaceName"))
+            .thenThrow(RuntimeException("error"))
+        viewModel.onSpaceNameChange("spaceName")
+        viewModel.createSpace()
+        assert(viewModel.state.value.error == "error")
+    }
+}
