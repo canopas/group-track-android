3aa9d27af280b51bbf517981240db6f29d0e51f5
Radhika Canopas <radhika.s@canopas.com>
2024-02-12 07:34:54 +0530

    WIP - cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// Import the Firebase SDK for Google Cloud Functions.
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Adds a message that welcomes new users into the chat.
+exports.deleteUserData = functions.firestore
+                           .document('users/{docId}')
+                           .onWrite((change, context) => {
+                                functions.logger.log('user updated ${snap.data()}');
+                            });
