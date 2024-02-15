d478df940893d6cd2c2360b657584bbd216a9b8e
Radhika canopas <74301808+cp-radhika-s@users.noreply.github.com>
2024-02-15 12:22:32 +0530

    Manage multiple sessions (#25)

--- app/src/test/java/com/canopas/yourspace/ui/MainViewModelTest.kt
+++ app/src/test/java/com/canopas/yourspace/ui/MainViewModelTest.kt
@@ -1,8 +1,15 @@
 package com.canopas.yourspace.ui
+
 import com.canopas.yourspace.MainCoroutineRule
+import com.canopas.yourspace.data.models.user.ApiUserSession
+import com.canopas.yourspace.data.service.auth.AuthService
+import com.canopas.yourspace.data.service.user.ApiUserService
 import com.canopas.yourspace.data.storage.UserPreferences
+import com.canopas.yourspace.data.utils.AppDispatcher
 import com.canopas.yourspace.ui.navigation.AppNavigator
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
@@ -20,12 +27,26 @@ class MainViewModelTest {
 
     private val userPreferences = mock<UserPreferences>()
     private val navigator = mock<AppNavigator>()
+    private val apiUserService = mock<ApiUserService>()
+    private val authService = mock<AuthService>()
+    private val testDispatcher = AppDispatcher(IO = UnconfinedTestDispatcher())
+
+    private fun setup() {
+        viewModel = MainViewModel(
+            userPreferences,
+            navigator,
+            testDispatcher,
+            apiUserService,
+            authService
+        )
+    }
 
     @Test
     fun `should navigate to sign-in screen if intro is shown and current user is null`() = runTest {
         whenever(userPreferences.isIntroShown()).thenReturn(true)
         whenever(userPreferences.currentUser).thenReturn(null)
-        viewModel = MainViewModel(userPreferences, navigator)
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf())
+        setup()
         verify(navigator).navigateTo(
             "sign-in",
             popUpToRoute = "intro",
@@ -38,7 +59,8 @@ class MainViewModelTest {
         whenever(userPreferences.currentUser).thenReturn(mock())
         whenever(userPreferences.isIntroShown()).thenReturn(true)
         whenever(userPreferences.isOnboardShown()).thenReturn(false)
-        viewModel = MainViewModel(userPreferences, navigator)
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf())
+        setup()
         verify(navigator).navigateTo(
             "onboard",
             popUpToRoute = "intro",
@@ -51,12 +73,82 @@ class MainViewModelTest {
         whenever(userPreferences.currentUser).thenReturn(mock())
         whenever(userPreferences.isIntroShown()).thenReturn(true)
         whenever(userPreferences.isOnboardShown()).thenReturn(true)
-
-        viewModel = MainViewModel(userPreferences, navigator)
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf())
+        setup()
         verify(navigator, times(0)).navigateTo(
             "onboard",
             popUpToRoute = "intro",
             inclusive = true
         )
+    }
+
+    @Test
+    fun `listenUserSession should emit sessionExpiredState when session is not active`() = runTest {
+        val session = ApiUserSession(user_id = "1", id = "1", session_active = true)
+        val updatedSession = ApiUserSession(user_id = "1", id = "1", session_active = false)
+
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf(session))
+        whenever(apiUserService.getUserSessionFlow("1", "1")).thenReturn(flowOf(updatedSession))
+        whenever(userPreferences.isIntroShown()).thenReturn(true)
+
+        setup()
+        verify(apiUserService).getUserSessionFlow("1", "1")
+        assert(viewModel.sessionExpiredState.value)
+    }
+
+    @Test
+    fun `listenUserSession should not emit sessionExpiredState when session is active`() = runTest {
+        val session = ApiUserSession(user_id = "1", id = "1", session_active = true)
+
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf(session))
+        whenever(apiUserService.getUserSessionFlow("1", "1")).thenReturn(flowOf(session))
+        whenever(userPreferences.isIntroShown()).thenReturn(true)
+
+        setup()
+        verify(apiUserService).getUserSessionFlow("1", "1")
+        assert(!viewModel.sessionExpiredState.value)
+    }
+
+    @Test
+    fun `listenUserSession should not emit sessionExpiredState when session is null`() = runTest {
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf(null))
+        whenever(userPreferences.isIntroShown()).thenReturn(true)
+
+        setup()
+        assert(!viewModel.sessionExpiredState.value)
+    }
+
+    @Test
+    fun `signOut should call authService signOut`() = runTest {
+        whenever(userPreferences.isIntroShown()).thenReturn(true)
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf(null))
+
+        setup()
+        viewModel.signOut()
+        verify(authService).signOut()
+    }
+
+    @Test
+    fun `signOut should navigate to sign-in screen`() = runTest {
+        whenever(userPreferences.isIntroShown()).thenReturn(true)
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf(null))
+
+        setup()
+        viewModel.signOut()
+        verify(navigator).navigateTo(
+            "sign-in",
+            "home",
+            true
+        )
+    }
+
+    @Test
+    fun `signOut should emit sessionExpiredState false`() = runTest {
+        whenever(userPreferences.isIntroShown()).thenReturn(true)
+        whenever(userPreferences.currentUserSessionState).thenReturn(flowOf(null))
+
+        setup()
+        viewModel.signOut()
+        assert(!viewModel.sessionExpiredState.value)
     }
 }
