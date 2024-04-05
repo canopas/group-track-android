58633eb99ddc544ae6d75ce45e4302f11fcc77af
Radhika Canopas <radhika.s@canopas.com>
2024-04-05 17:48:18 +0530

    Add fcm dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -125,7 +125,10 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.10.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
 
     // Test
     testImplementation("junit:junit:4.13.2")
