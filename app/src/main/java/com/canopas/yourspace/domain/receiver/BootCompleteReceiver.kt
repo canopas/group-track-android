b7b399d4cfc34613f847ad978d8908120e43b696
Radhika Canopas <radhika.s@canopas.com>
2024-05-08 12:07:59 +0530

    Minor fix

--- app/src/main/java/com/canopas/yourspace/domain/receiver/BootCompleteReceiver.kt
+++ app/src/main/java/com/canopas/yourspace/domain/receiver/BootCompleteReceiver.kt
@@ -3,6 +3,7 @@ package com.canopas.yourspace.domain.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.canopas.yourspace.data.service.auth.AuthService
 import com.canopas.yourspace.data.service.location.LocationManager
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -13,8 +14,11 @@ class BootCompleteReceiver : BroadcastReceiver() {
     @Inject
     lateinit var locationManager: LocationManager
 
+    @Inject
+    lateinit var authService: AuthService
+
     override fun onReceive(context: Context, intent: Intent) {
-        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action && authService.currentUser != null) {
             locationManager.startService()
         }
     }
