386a0c8cada0a9e8ff95e8f4b38c28c59d27e7bf
kaushiksaliya <kaushik.s@canopas.com>
2024-05-27 17:50:43 +0530

    Redesign map tag and bottom member view

--- app/src/main/java/com/canopas/yourspace/ui/flow/home/map/component/MapUserItem.kt
+++ app/src/main/java/com/canopas/yourspace/ui/flow/home/map/component/MapUserItem.kt
@@ -5,20 +5,14 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.canopas.yourspace.R
 import com.canopas.yourspace.data.models.user.UserInfo
@@ -33,25 +27,11 @@ fun MapUserItem(
 ) {
     val user = userInfo.user
     Column(
-        modifier = Modifier.width(60.dp)
-            .clickable {
-                onClick()
-            }
-            .padding(vertical = 10.dp),
+        modifier = Modifier
+            .clickable { onClick() },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        UserProfile(
-            Modifier
-                .size(40.dp),
-            user = user
-        )
-        Spacer(modifier = Modifier.height(2.dp))
-        Text(
-            text = user.first_name ?: "",
-            overflow = TextOverflow.Ellipsis,
-            maxLines = 1,
-            style = AppTheme.appTypography.caption.copy(color = AppTheme.colorScheme.textPrimary)
-        )
+        UserProfile(Modifier.size(40.dp), user = user)
     }
 }
 
@@ -61,8 +41,7 @@ fun AddMemberBtn(
     onClick: () -> Unit
 ) {
     Column(
-        modifier = Modifier
-            .clickable { onClick() }.padding(10.dp),
+        modifier = Modifier.clickable { onClick() },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
@@ -72,10 +51,8 @@ fun AddMemberBtn(
                     1.dp,
                     AppTheme.colorScheme.primary.copy(alpha = 0.7f),
                     shape = CircleShape
-                ).background(
-                    AppTheme.colorScheme.surface,
-                    shape = CircleShape
-                ),
+                )
+                .background(AppTheme.colorScheme.surface, shape = CircleShape),
             contentAlignment = Alignment.Center
         ) {
             if (showLoader) {
@@ -89,10 +66,5 @@ fun AddMemberBtn(
                 )
             }
         }
-        Spacer(modifier = Modifier.height(2.dp))
-        Text(
-            text = stringResource(id = R.string.common_btn_add),
-            style = AppTheme.appTypography.caption.copy(color = AppTheme.colorScheme.textPrimary)
-        )
     }
 }
