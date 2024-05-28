73d82afc3940f8602b3a0355bba5aa34c7899aac
kaushiksaliya <kaushik.s@canopas.com>
2024-05-28 12:00:34 +0530

    Fix to show member with name

--- app/src/main/java/com/canopas/yourspace/ui/flow/home/map/component/MapUserItem.kt
+++ app/src/main/java/com/canopas/yourspace/ui/flow/home/map/component/MapUserItem.kt
@@ -5,14 +5,20 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.canopas.yourspace.R
 import com.canopas.yourspace.data.models.user.UserInfo
@@ -28,10 +34,18 @@ fun MapUserItem(
     val user = userInfo.user
     Column(
         modifier = Modifier
+            .width(40.dp)
             .clickable { onClick() },
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         UserProfile(Modifier.size(40.dp), user = user)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            text = user.first_name ?: "",
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            style = AppTheme.appTypography.caption.copy(color = AppTheme.colorScheme.textPrimary)
+        )
     }
 }
 
@@ -66,5 +80,10 @@ fun AddMemberBtn(
                 )
             }
         }
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            text = stringResource(id = R.string.common_btn_add),
+            style = AppTheme.appTypography.caption.copy(color = AppTheme.colorScheme.textPrimary)
+        )
     }
 }
