af4777346192d311d41e051571361ca729d9b7d9
kaushiksaliya <kaushik.s@canopas.com>
2024-05-30 14:19:53 +0530

    Minor

--- app/src/main/java/com/canopas/yourspace/ui/flow/home/map/component/MapUserItem.kt
+++ app/src/main/java/com/canopas/yourspace/ui/flow/home/map/component/MapUserItem.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
