9f04b71e0086337e1168d3dc39e1a25d87140fb8
kaushiksaliya <140153665+kaushiksaliya@users.noreply.github.com>
2024-06-03 11:27:22 +0530

    Redesign intro screen (#62)
    * Redesign intro screen and sigh in methode screen
    
    * Fix lint

--- app/src/main/java/com/canopas/yourspace/ui/flow/auth/methods/SignInMethodsScreen.kt
+++ app/src/main/java/com/canopas/yourspace/ui/flow/auth/methods/SignInMethodsScreen.kt
@@ -14,30 +14,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.canopas.yourspace.R
 import com.canopas.yourspace.ui.component.AppBanner
 import com.canopas.yourspace.ui.component.AppLogo
-import com.canopas.yourspace.ui.component.AppProgressIndicator
+import com.canopas.yourspace.ui.component.PrimaryButton
+import com.canopas.yourspace.ui.component.PrimaryOutlinedButton
 import com.canopas.yourspace.ui.theme.AppTheme
 import com.canopas.yourspace.ui.theme.CatchMeTheme
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -48,10 +43,7 @@ import timber.log.Timber
 @Composable
 fun SignInMethodsScreen() {
     Scaffold {
-        SignInContent(
-            modifier = Modifier
-                .padding(it)
-        )
+        SignInContent(modifier = Modifier.padding(it))
     }
 }
 
@@ -81,7 +73,7 @@ private fun SignInContent(modifier: Modifier) {
         AppLogo(colorTint = AppTheme.colorScheme.primary)
         Spacer(modifier = Modifier.weight(1f))
         GoogleSignInBtn()
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(24.dp))
         PhoneLoginBtn(onClick = { viewModel.signInWithPhone() })
         Spacer(modifier = Modifier.weight(1f))
     }
@@ -96,22 +88,11 @@ private fun SignInContent(modifier: Modifier) {
 
 @Composable
 private fun PhoneLoginBtn(onClick: () -> Unit) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth(fraction = 0.8f),
-        shape = RoundedCornerShape(50),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = AppTheme.colorScheme.primary
-        )
-    ) {
-        Text(
-            text = stringResource(id = R.string.sign_in_btn_continue_with_phone),
-            style = AppTheme.appTypography.label1.copy(color = AppTheme.colorScheme.onPrimary),
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(vertical = 6.dp)
-        )
-    }
+    PrimaryButton(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+        label = stringResource(id = R.string.sign_in_btn_continue_with_phone),
+        onClick = onClick
+    )
 }
 
 @Composable
@@ -134,7 +115,7 @@ private fun GoogleSignInBtn() {
             }
         }
 
-    Button(
+    PrimaryOutlinedButton(
         onClick = {
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(context.getString(R.string.default_web_client_id))
@@ -146,28 +127,18 @@ private fun GoogleSignInBtn() {
             signInClientLauncher.launch(googleSignInClient.signInIntent)
         },
         modifier = Modifier
-            .fillMaxWidth(fraction = 0.8f),
-        shape = RoundedCornerShape(50),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.White
-        )
-    ) {
-        if (state.showGoogleLoading) {
-            AppProgressIndicator()
-        } else {
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        showLoader = state.showGoogleLoading,
+        label = stringResource(id = R.string.sign_in_btn_continue_with_google),
+        icon = {
             Image(
                 painter = painterResource(id = R.drawable.ic_sign_in_google_logo),
                 contentDescription = null,
                 modifier = Modifier.size(18.dp)
             )
-            Text(
-                text = stringResource(id = R.string.sign_in_btn_continue_with_google),
-                style = AppTheme.appTypography.label1.copy(color = Color.Black),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(vertical = 6.dp, horizontal = 6.dp)
-            )
         }
-    }
+    )
 }
 
 @Preview
