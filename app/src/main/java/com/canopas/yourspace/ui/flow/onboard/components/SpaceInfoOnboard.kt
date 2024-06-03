7215419983d5ec93a62c19584f6da0bc3e1161cf
kaushiksaliya <kaushik.s@canopas.com>
2024-06-03 16:28:00 +0530

    Minor

--- app/src/main/java/com/canopas/yourspace/ui/flow/onboard/components/SpaceInfoOnboard.kt
+++ app/src/main/java/com/canopas/yourspace/ui/flow/onboard/components/SpaceInfoOnboard.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
