4684a6d38ff9060467755bf0e0e72995daa6e632
Sneh <sneh.s@canopas.com>
2024-08-23 17:40:57 +0530

    Showed timeline's list based on particular date.

--- app/src/main/java/com/canopas/yourspace/ui/flow/journey/timeline/JourneyTimelineViewModel.kt
+++ app/src/main/java/com/canopas/yourspace/ui/flow/journey/timeline/JourneyTimelineViewModel.kt
@@ -199,8 +199,6 @@ class JourneyTimelineViewModel @Inject constructor(
         }
         calendar.set(Calendar.HOUR_OF_DAY, 0)
         calendar.set(Calendar.MINUTE, 0)
-        calendar.set(Calendar.SECOND, 0)
-        calendar.set(Calendar.MILLISECOND, 0)
         val timestampFrom = calendar.timeInMillis
 
         calendar.set(Calendar.HOUR_OF_DAY, 23)
