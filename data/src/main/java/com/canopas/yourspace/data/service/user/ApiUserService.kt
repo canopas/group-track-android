ea224465b8f65916ecbd10a1d0e9d0b7596aa91c
cp-megh <megh.l@canopas.com>
2024-10-09 15:02:55 +0530

    minor changes

--- data/src/main/java/com/canopas/yourspace/data/service/user/ApiUserService.kt
+++ data/src/main/java/com/canopas/yourspace/data/service/user/ApiUserService.kt
@@ -2,6 +2,7 @@ package com.canopas.yourspace.data.service.user
 
 import com.canopas.yourspace.data.models.user.ApiUser
 import com.canopas.yourspace.data.models.user.ApiUserSession
+import com.canopas.yourspace.data.models.user.LOGIN_TYPE_APPLE
 import com.canopas.yourspace.data.models.user.LOGIN_TYPE_GOOGLE
 import com.canopas.yourspace.data.service.location.ApiLocationService
 import com.canopas.yourspace.data.utils.Config
@@ -32,7 +33,8 @@ class ApiUserService @Inject constructor(
 ) {
     private val userRef = db.collection(FIRESTORE_COLLECTION_USERS)
     private fun sessionRef(userId: String) =
-        userRef.document(userId.takeIf { it.isNotBlank() } ?: "null").collection(Config.FIRESTORE_COLLECTION_USER_SESSIONS)
+        userRef.document(userId.takeIf { it.isNotBlank() } ?: "null")
+            .collection(Config.FIRESTORE_COLLECTION_USER_SESSIONS)
 
     suspend fun getUser(userId: String): ApiUser? {
         return try {
@@ -74,12 +76,13 @@ class ApiUserService @Inject constructor(
         } else {
             val user = ApiUser(
                 id = uid!!,
-                email = account?.email ?: "",
-                auth_type = if (account != null) LOGIN_TYPE_GOOGLE else 0,
+                email = account?.email ?: firebaseUser?.email ?: "",
+                auth_type = if (account != null) LOGIN_TYPE_GOOGLE else if (firebaseUser != null) LOGIN_TYPE_APPLE else 0,
                 first_name = account?.givenName ?: firebaseUser?.displayName ?: "",
                 last_name = account?.familyName ?: "",
                 provider_firebase_id_token = firebaseToken,
-                profile_image = account?.photoUrl?.toString() ?: firebaseUser?.photoUrl?.toString() ?: ""
+                profile_image = account?.photoUrl?.toString() ?: firebaseUser?.photoUrl?.toString()
+                    ?: ""
             )
             userRef.document(uid).set(user).await()
             val sessionDocRef = sessionRef(user.id).document()
